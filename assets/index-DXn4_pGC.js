dd6ccdb14373f9bd60032a1f672e4d287ae329ec
davim187 <daviimorais39@gmail.com>
2024-05-31 13:17:21 -0300

    Updates

--- assets/index-DXn4_pGC.js
+++ assets/index-DXn4_pGC.js
@@ -458,7 +458,7 @@ Error generating stack: `+i.message+`
   &:hover {
     animation: ${Rx} 1.5s forwards;
   }
-`;function Ox(e){return C.jsx(Tx,{nameButton:e.nameButton,children:e.nameButton})}const jx=F.div`
+`;function Ox(e){return C.jsx(Tx,{nameButton:e.nameButton,onClick:e.onClick,target:e.target,children:e.nameButton})}const jx=F.div`
   display: flex;
   width: 100%;
   height: 100vh;
@@ -487,7 +487,7 @@ Error generating stack: `+i.message+`
     font-size: 3.5vw;
   }
   }
-`;function Nx(){return C.jsxs(jx,{children:[C.jsx("h1",{children:"Sobre:"}),C.jsx("h3",{children:"Minha trajetória iníciou com o certificado de Técnico em Redes de Computadores no estado do Ceará. Especialização não foi aplicado, mas com ela conseguir ingressar na area da TI, com conhecimentos para Front-end e Back-end. Cursando Análise e Desenvolvimento de Sistemas no Centro Universitário Ateneu. Tenho interesse em aprender novas tecnologias e criar soluções inovadoras para os desafios do mercado. Desde março de 2023, trabalho como desenvolvedor e suporte na empresa Tijuca Alimentos, presto suporte ao sistema e desenvolvo projetos de interface de programação de aplicativos (API) Node.js, e aplicações usando o React. Antes disso, fiz um estágio de quatro meses na mesma empresa, onde participei de quatro projetos de desenvolvimento web, sendo reconhecido pela qualidade e agilidade do meu trabalho."}),C.jsx(Ox,{nameButton:"Curriculo"})]})}const bx=F.div`
+`;function Nx(){return C.jsxs(jx,{children:[C.jsx("h1",{children:"Sobre:"}),C.jsx("h3",{children:"Minha trajetória iníciou com o certificado de Técnico em Redes de Computadores no estado do Ceará. Especialização não foi aplicado, mas com ela conseguir ingressar na area da TI, com conhecimentos para Front-end e Back-end. Cursando Análise e Desenvolvimento de Sistemas no Centro Universitário Ateneu. Tenho interesse em aprender novas tecnologias e criar soluções inovadoras para os desafios do mercado. Desde março de 2023, trabalho como desenvolvedor e suporte na empresa Tijuca Alimentos, presto suporte ao sistema e desenvolvo projetos de interface de programação de aplicativos (API) Node.js, e aplicações usando o React. Antes disso, fiz um estágio de quatro meses na mesma empresa, onde participei de quatro projetos de desenvolvimento web, sendo reconhecido pela qualidade e agilidade do meu trabalho."}),C.jsx(Ox,{nameButton:"Curriculo",target:"_blank",onClick:()=>window.document.location="https://drive.google.com/file/d/1F_A3rHqk4JDfUIkEaxKuUVXH73rNzcvE/view?usp=sharing"})]})}const bx=F.div`
   display: flex;
   flex-direction: row;
   flex-wrap: wrap;
